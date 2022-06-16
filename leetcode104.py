72bf656f5a4149ef410f3692bfa9b4269c2864ec
sagarhasan273 <sagarhasan273@gmail.com>
2022-06-16 18:27:42 +0600

    DFS iterative solution

--- leetcode104.py
+++ leetcode104.py
@@ -9,18 +9,14 @@ class Solution:
         if not root:
             return 0
         
-        level = 0
-        q = deque([root])
-        
-        while q:
+        level = 1
+        stack = [[root, 1]]
+        while stack:
+            node, depth = stack.pop()
             
-            for i in range(len(q)):
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-                    
-            level += 1
-
+            if node:
+                level = max(level, depth)
+                stack.append([node.left, depth+1])
+                stack.append([node.right, depth+1])
+        
         return level
