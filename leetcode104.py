966337c149db3f4a3bcd933767e57ee01d7fc822
sagarhasan273 <sagarhasan273@gmail.com>
2022-06-16 17:58:05 +0600

    BFS iterative solution

--- leetcode104.py
+++ leetcode104.py
@@ -6,6 +6,21 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if root:
-            return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-        return 0
+        if not root:
+            return 0
+        
+        level = 0
+        q = deque([root])
+        
+        while q:
+            
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                    
+            level += 1
+
+        return level
