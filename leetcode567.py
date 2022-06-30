81d322e14356d448352501e3ff180ed5d98f95da
sagarhasan273 <sagarhasan273@gmail.com>
2022-06-30 16:52:59 +0600

    567. Permutation in String

---
+++ leetcode567.py
@@ -0,0 +1,35 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        s1_count, s2_count = [0]*26, [0]*26
+        for i in range(len(s1)):
+            s1_count[ord(s1[i]) - ord('a')] += 1
+            s2_count[ord(s2[i]) - ord('a')] += 1
+        
+        matches = 0
+        for i in range(26):
+            matches += 1 if s1_count[i] == s2_count[i] else 0
+        
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+            
+            index = ord(s2[l]) - ord('a')
+            s2_count[index] -= 1
+            if s1_count[index] == s2_count[index]:
+                matches += 1
+            elif s1_count[index] - 1 == s2_count[index]:
+                matches -= 1
+            l += 1
+            
+            index = ord(s2[r]) - ord('a')
+            s2_count[index] += 1
+            if s1_count[index] == s2_count[index]:
+                matches += 1
+            elif s1_count[index] + 1 == s2_count[index]:
+                matches -= 1
+        
+        return matches == 26
