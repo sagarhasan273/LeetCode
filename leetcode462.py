db6206c2be089ee1d6962ba1f9d51f58b18bfaf5
sagarhasan273 <sagarhasan273@gmail.com>
2022-06-30 14:05:39 +0600

    462. Minimum Moves to Equal Array Elements II

--- leetcode462.py
+++ leetcode462.py
@@ -1,20 +1,10 @@
 class Solution:
     def minMoves2(self, nums: List[int]) -> int:
         nums.sort()
-        if len(nums)%2 == 1:
-            m = len(nums)//2
-            total = 0
-            for i in nums:
-                total += abs(i - nums[m])
-            return total
-        else:
-            m = (len(nums)//2) - 1
-            m0 = m + 1
-            totalm, totalm0 = 0, 0
-            
-            for i in nums:
-                totalm += abs(i-nums[m])
-                totalm0 += abs(i-nums[m0])
-            
-            return min(totalm, totalm0)
         
+        m = len(nums)//2
+        total = 0
+        for i in nums:
+            total += abs(i - nums[m])
+        return total
+       
