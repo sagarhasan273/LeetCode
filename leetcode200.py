b0346160a63423a12222fa4ca272e848ee0223da
sagarhasan273 <sagarhasan273@gmail.com>
2022-07-02 22:33:45 +0600

    200. Number of Islands | BFS solution

--- leetcode200.py
+++ leetcode200.py
@@ -1,22 +1,24 @@
 class Solution:
+    
     def numIslands(self, grid: List[List[str]]) -> int:
         island = 0
         rows, cols = len(grid), len(grid[0])
         
-        def dfs(r, c):
-            if r not in range(rows) or c not in range(cols):
-                return
-            if grid[r][c] == '1':
-                grid[r][c] = '#'
-                dfs(r+1, c)
-                dfs(r-1, c)
-                dfs(r, c+1)
-                dfs(r, c-1)
+        def bfs(r, c):
+            q = collections.deque()
+            q.append((r, c))
+            while q:
+                row, col = q.popleft()
+                for dr, dc in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+                    r, c = row+dr, col+dc
+                    if r in range(rows) and c in range(cols) and grid[r][c] == '1':
+                        grid[r][c] = '#'
+                        q.append((r, c))
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == '1':
                     island += 1
-                    dfs(r, c)
+                    bfs(r, c)
         
         return island
