ea01e4544b3b6102653c3a9ca9770aa57a0e1185
sagarhasan273 <sagarhasan273@gmail.com>
2022-07-14 17:59:02 +0600

    DP Memoization || 42 ms, faster than 72.41% of Python3 online submissions for Decode Ways.

--- leetcode91.py
+++ leetcode91.py
@@ -1,15 +1,18 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        l = len(s)  
+        l = len(s)
         dp = {l: 1}
         
-        for i in range(l-1, -1, -1):
+        def dfs(i):
+            if i in dp:
+                return dp[i]
             if s[i] == "0":
-                dp[i] = 0
-            else:
-                dp[i] = dp[i+1]
+                return 0
             
-            if i+1 < l and (s[i] == "1" or (s[i] == '2' and s[i+1] in "0123456")):
-                dp[i] += dp[i+2]
+            res = dfs(i+1)
+            if (i+1 < l) and (s[i] == "1" or (s[i] == "2" and s[i+1] in "0123456")):
+                res += dfs(i+2)
+            dp[i] = res
+            return res
         
-        return dp[0]
+        return dfs(0)
