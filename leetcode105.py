00e87e472a6526999697be9085f58f6486fd0f27
sagarhasan273 <sagarhasan273@gmail.com>
2022-07-14 16:25:45 +0600

    Construct Binary Tree || 283 ms, faster than 35.00% of Python3 online submissions for Construct Binary Tree from Preorder and Inorder Traversal.

--- leetcode105.py
+++ leetcode105.py
@@ -6,10 +6,14 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder:
+        def dfs(preorder, inorder):
+            if preorder and inorder:
+                node = TreeNode(preorder[0])
+                i = inorder.index(preorder[0])
+                node.left = dfs(preorder[1:i+1], inorder[:i])
+                node.right = dfs(preorder[i+1:], inorder[i+1:])
+                return node
+
             return None
-        root = TreeNode(preorder[0])
-        mid = inorder.index(preorder[0])
-        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
-        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
-        return root
+        
+        return dfs(preorder, inorder)
