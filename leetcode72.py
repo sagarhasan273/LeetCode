78e9e8f0386154551d536be5ac033a8062a5af87
sagarhasan273 <sagarhasan273@gmail.com>
2022-07-19 12:26:50 +0600

    93 ms, faster than 98.27% of Python3 online submissions for Edit Distance.

--- leetcode72.py
+++ leetcode72.py
@@ -1,18 +1,19 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        dp=[[-1]*(len(word2)+1) for _ in range(len(word1)+1)]
-        
+        w1, w2 = len(word1), len(word2)
+        dp=[[-1]*(w2+1) for _ in range(w1+1)]
+
         def dfs(i,j):
-            if i==len(word1):
-                return len(word2)-j
+            if i == w1:
+                return w2-j
             
-            if j==len(word2):
-                return len(word1)-i
+            if j == w2:
+                return w1-i
             
-            if(dp[i][j]!=-1):
+            if dp[i][j] != -1:
                 return dp[i][j]
             
-            if(word1[i]==word2[j]):
+            if word1[i] == word2[j]:
                 dp[i][j] = dfs(i+1,j+1)
                 return dp[i][j]
             
