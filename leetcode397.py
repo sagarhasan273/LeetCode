7adc899243c550292afa2e166c24a3193a11677f
sagarhasan273 <sagarhasan273@gmail.com>
2022-07-19 22:38:48 +0600

    38 ms, faster than 83.21% of Python3 online submissions for Integer Replacement.

--- leetcode397.py
+++ leetcode397.py
@@ -10,10 +10,10 @@ class Solution:
                 return memo[n]
             
             if n%2:
-                memo[n] = 1 + min(1+self.integerReplacement((n+1)//2), 1+self.integerReplacement((n-1)//2))
+                memo[n] = 1 + min(1+dfs((n+1)//2), 1+dfs((n-1)//2))
                 return memo[n]
             
-            memo[n] = 1 + self.integerReplacement(n//2)
+            memo[n] = 1 + dfs(n//2)
             return memo[n]
         
         return dfs(n)
